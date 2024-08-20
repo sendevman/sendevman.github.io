e712cca95708773dc7c3676a946f57159af662dc
sendevman <devman.sd@gmail.com>
2024-08-20 00:09:04 -0700

    Updates

---
+++ precache-manifest.59205481f5994cd3432880800ba27b28.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0463b0d771abaf44c8d1085c41ed841f",
+    "url": "https://sendevman.github.io/index.html"
+  },
+  {
+    "revision": "46326f00cbc060fec8c6",
+    "url": "https://sendevman.github.io/static/css/2.d64471e2.chunk.css"
+  },
+  {
+    "revision": "23e5314e890eeb51bc0d",
+    "url": "https://sendevman.github.io/static/css/main.db76a3b0.chunk.css"
+  },
+  {
+    "revision": "46326f00cbc060fec8c6",
+    "url": "https://sendevman.github.io/static/js/2.5bff3dec.chunk.js"
+  },
+  {
+    "revision": "7c4b6536cab2d1970758ff058c5fbbf8",
+    "url": "https://sendevman.github.io/static/js/2.5bff3dec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "23e5314e890eeb51bc0d",
+    "url": "https://sendevman.github.io/static/js/main.0ad1cfe4.chunk.js"
+  },
+  {
+    "revision": "c85278d00a7ff7b2a747",
+    "url": "https://sendevman.github.io/static/js/runtime-main.25586bf1.js"
+  }
+]);
