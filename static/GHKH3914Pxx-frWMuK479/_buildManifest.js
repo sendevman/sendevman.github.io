4ea080116e7d8befdbcf4f41862b1232a2427486
sendevman <devman.sd@gmail.com>
2024-08-20 02:52:38 -0700

    Updates

--- static/GHKH3914Pxx-frWMuK479/_buildManifest.js
+++
@@ -1,1 +0,0 @@
-self.__BUILD_MANIFEST={__rewrites:{beforeFiles:[],afterFiles:[],fallback:[]},"/":["static/chunks/953-723ccd7126177e80.js","static/chunks/pages/index-3ff254bf00729bcb.js"],"/_error":["static/chunks/pages/_error-54de1933a164a1ff.js"],sortedPages:["/","/_app","/_error"]},self.__BUILD_MANIFEST_CB&&self.__BUILD_MANIFEST_CB();
